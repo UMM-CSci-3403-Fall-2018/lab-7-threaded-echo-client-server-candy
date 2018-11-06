317b8312f7d9e32c9dd2c16b15836832d079a6d4
lalys <xion1337@morris.umm.edu>
2018-11-06 16:13:03 -0600

    Correct minor errors in KeyboardReader and ScreenWriter. All client tests pass.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -4,81 +4,70 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class EchoClient {
 	public static final int PORT_NUMBER = 6013;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		EchoClient client = new EchoClient();
 		client.start();
 	}
 
-	Public class keyboardReader implements runnable{
-		OutputStream stream;
-		Socket socket;
+	private class KeyboardReader implements Runnable {
+	    OutputStream stream;
 
-		public keyboardReader(Socket socket) throws IOException{
-			try{
-			this.output = socket.getOutputStream;
-			this.socket = socket();
-		}
-		catch(IOException ioe){
-			System.out.println("The keyboardReader has an error");
-			System.out.println(ioe);
-			}
-		}
-	}
+	    public KeyboardReader(OutputStream stream) {
+	       this.stream = stream;
+        }
 		public void run(){
-			try{
 				int readByte;
+				try{
 				while((readByte = System.in.read()) != -1){
-					output.write(readByte);
+					stream.write(readByte);
 				}
 			}
-				catch(IOException ioe){
+				catch (IOException ioe){
 					System.out.println("Error");
 					System.out.println(ioe);
 			}
 		}
+	}
 
-		public class screenWriter implements runnable{
+
+		private class ScreenWriter implements Runnable{
 			InputStream stream;
 
-			public screenWriter(InputStream stream){
+			public ScreenWriter(InputStream stream){
 				this.stream = stream;
 			}
+
 			public void run(){
 				int inByte;
 				try{
-					while ((inbyte = stream.read()) != -1){
+				while ((inByte = stream.read()) != -1){
 						System.out.write(inByte);
 					}
-				}
-				catch(IOException ioe){
+				}catch (IOException ioe){
 					System.out.println("Error");
 					System.out.println(ioe);
 			}
 		}
-
-		private void start() throws IOException, InterruptedException {
-			Socket socket = new Socket("localhost", PORT_NUMBER);
-
-			InputStream socketInputStream = socket.getInputStream();
-			Thread inputThread = new Thread(new ScreenWriter(socketInputStream));
-
-			OutputStream socketOutputStream = socket.getOutputStream();
-			Thread outputThread = new Thread(new KeyboardReader(socketOutputStream));
-
-			outputThread.start();
-			inputThread.start();
-			outputThread.join();
-
-					socket.shutdownOutput();
-	        inputThread.join();
-	        System.out.flush();
-	        socket.shutdownInput();
-
-	    }
 	}
+
+
+	private void start() throws IOException, InterruptedException {
+		Socket socket = new Socket("localhost", PORT_NUMBER);
+		InputStream socketInputStream = socket.getInputStream();
+		Thread inputThread = new Thread(new ScreenWriter(socketInputStream));
+		OutputStream socketOutputStream = socket.getOutputStream();
+		Thread outputThread = new Thread(new KeyboardReader(socketOutputStream));
+		outputThread.start();
+		inputThread.start();
+		outputThread.join();
+        socket.shutdownOutput();
+        inputThread.join();
+        System.out.flush();
+        socket.shutdownInput();
+
+    }
 }
